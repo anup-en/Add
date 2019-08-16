fa940604c2f7e723c1caeff03e7baa19850bfe08
anup-en <54133939+anup-en@users.noreply.github.com>
2019-08-16 08:49:39 +0530

    Add files via upload

---
+++ leap_year.py
@@ -0,0 +1,7 @@
+a=int(input('Enter the year:'))
+if( (a/4==0 and a/400==0) or (a%4==0) ):
+   print("Entered year is a LEAP year:")
+else:
+    print('Entered year is not an LEAP year')
+      
+      
---
+++ while_(100-10).py
@@ -0,0 +1,4 @@
+a=100
+while(a>=10):
+    print(a)
+    a=a-10
